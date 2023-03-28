9378ab4ed0d9e0c51b82290b0d156c0bdb274bd4
Dmitrii Palienko <dmitrii.palienko@gmail.com>
2023-03-28 17:20:32 +0300

    Fixing typos

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 from collections import OrderedDict
 from pandas import DataFrame
 
+link = "https://a-z-animals.com/animals/"
+final_list_of_animals = []
+dict_of_all_animals_properties = {}
 headers = {
 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
 "Accept-Encoding": "gzip, deflate",
@@ -11,13 +14,16 @@ headers = {
 "Upgrade-Insecure-Requests": "1",
 "User-Agent": "Mozilla/5.0 (Linux; Android 10; SM-G996U Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Mobile Safari/537.36",
 }
+
+
 # Getting HTML text of a website
-def get_html(link):
+def get_html_of_animals(link: str) -> str:
     html_ver = requests.get(link, headers=headers)
     return html_ver.text
 
+
 # Getting a list of links to animal pages
-def get_list_of_animal_links(html_text):
+def get_list_of_animals_link(html_text: str) -> list:
    try:
     array_of_animals = []
     soup = BeautifulSoup(html_text, 'html.parser')
@@ -28,19 +34,20 @@ def get_list_of_animal_links(html_text):
    except Exception as e:
        pass
 
-# Getting a name of each animal
-def get_title_of_animal(link):
+# Getting a name of a specific animal
+def get_title_of_animal(link: str) -> str:
    try:
-    soup = BeautifulSoup(get_html(link), 'html.parser')
+    soup = BeautifulSoup(get_html_of_animals(link), 'html.parser')
     title = soup.find('div', 'col-12').h1.text
     return title
    except Exception as e:
        pass
 
-#Function that allows to get all the facts from specific animal
-def get_facts(link):
+
+# Getting all the facts from a specific animal
+def get_facts_of_animal(link: str) -> OrderedDict:
    try:
-    soup = BeautifulSoup(get_html(link), 'html.parser')
+    soup = BeautifulSoup(get_html_of_animals(link), 'html.parser')
     property = []
     description_of_property = []
     title_animal = get_title_of_animal(link)
@@ -62,35 +69,44 @@ def get_facts(link):
    except Exception as e:
        pass
 
-#All avaible proporties of animals
-dict_of_properties = {}
 
-#Calling function to get whole list of links to animal pages
-try:
-    final_list_of_animals = []
-    link = "https://a-z-animals.com/animals/"
-    html_source_of_links = get_html(link)
-    list_of_animal_links = get_list_of_animal_links(html_source_of_links)
-except Exception as e:
-    pass
-#Using for loop to call function "get_fact" for each animal
-for animal in list_of_animal_links:
+# Getting list of all links corresponding to animal pages
+def get_list_of_all_animals_link() -> list:
     try:
-        facts = get_facts(animal)
-        for i, k in facts.items():
-            dict_of_properties.setdefault(i)
-        print(facts)
-        final_list_of_animals.append(facts)
+        html_source_of_links = get_html_of_animals(link)
+        list_of_animal_links = get_list_of_animals_link(html_source_of_links)
+        return list_of_animal_links
     except Exception as e:
         pass
 
-#Deleting None objects
-finished_work = list(filter((None).__ne__, final_list_of_animals))
 
-#Creating Dataset and importing it to CSV file
-try:
-    df = DataFrame(finished_work, columns=dict_of_properties.keys())
-    df.to_csv('Animal.csv')
-except Exception as e:
-    pass
+# Getting list related to all facts of all the animals
+def get_facts_of_all_animals_to_dict():
+    list_of_animal_links = get_list_of_all_animals_link()
+    for animal in list_of_animal_links:
+        try:
+            facts = get_facts_of_animal(animal)
+            for i, k in facts.items():
+                dict_of_all_animals_properties.setdefault(i)
+                # Used for showcasing facts in realtime
+                # print(facts)
+            final_list_of_animals.append(facts)
+        except Exception as e:
+            pass
 
+# Saving List to csv file using Dict properties (in project folder)
+def saving_to_csv() -> DataFrame.to_csv:
+    """Deleting None objects"""
+    finished_work = list(filter((None).__ne__, final_list_of_animals))
+
+    """Creating Dataset and importing it to CSV file"""
+    try:
+        df = DataFrame(finished_work, columns=dict_of_all_animals_properties.keys())
+        return df.to_csv('Animal.csv')
+    except Exception as e:
+        pass
+
+
+if __name__ == "__main__":
+    get_facts_of_all_animals_to_dict()
+    saving_to_csv()
